4eb2dbb7e2592892dfd5e420bcda76996099e8ac
wabernathy96 <wabernathy96@gmail.com>
2018-04-25 23:10:07 -0500

    Added paths to html files on htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 module.exports = (app) => {
     
+    // Get home html file for default page
     app.get ('/', (req,res) => {
-        
+        res.sendFile(path.join(__dirname, '../public/home.html'));
     });
 
+    // Get survey html file for default page
     app.get ('/survey', (req,res) => {
-
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
      
 };
