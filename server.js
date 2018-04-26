6bfedea02d8390fa7cd77fd9d893501a18c90109
wabernathy96 <wabernathy96@gmail.com>
2018-04-25 22:39:39 -0500

    Added code to server.js, server is listening

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+let app = express();
+let PORT = 3000;
+
+// Allow access to the public file
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Body-parser middleware for incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Join app routes
+// require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+// require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start server listening on PORT 3000
+app.listen(PORT, () => {
+  console.log(`Friend Finder listening on port: ${PORT}`);
+});
+
+
+
