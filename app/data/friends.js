ac21dc838666ad5c10827841c1946b7246da27b3
wabernathy96 <wabernathy96@gmail.com>
2018-04-30 20:55:24 -0500

    Added logic to apiRoutes, added module to html

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ let friendArr = [
         name: 'Test1',
         photo: 'https://www.pcr-online.biz/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_350/MTUxOTMyNDEwMjc0MjYwMzk1/1-clippyjpg.webp',
         scores: [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
         ]
     }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,33 @@ module.exports = (app) => {
     });
 
     app.post ('/api/friends', (req,res) => {
-        var userInput = req.body;
-		console.log('userInput = ' + JSON.stringify(userInput));
+        let userInput = req.body;
+
+        let userScores = userInput.scores;
+        
+        let matchName = '';
+        let matchImg = '';
+        let totDiff = 1000;
+
+        for(var i=0;i<friends.length;i++){
+            // console.log(`friend = ${JSON.stringify(friends[i])}`)
+
+            let diff = 0;
+            
+            for(let j=0;j<userScores;j++){
+                diff += Math.abs(friends[i].scores[j] - userScores[j]);
+            }
+
+            if (diff < totDiff) {
+                totalDiff = diff;
+                matchName = friends[i].name;
+                matchImg = friends[i].photo;
+            }
+        }
+        friends.push(userInput);
+
+        res.json({status: 'Working', matchName: matchName,  matchImg: matchImg});
     });
+
+    
 };
